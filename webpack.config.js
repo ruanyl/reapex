c2d61a0ddf4cd417916928edef0da5f1426ccf43
ruanyl <ruanyu1@gmail.com>
2019-04-17 22:46:18 +0300

    use webpack dll plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const glob = require('glob')
+const packages = require('./package.json')
+const manifest = path.resolve(__dirname, "dist/manifest.json")
 
 let componentEntries = []
 const registeredComponents = ['Counter', 'Hello', 'Nav'].forEach(comp => {
@@ -11,29 +13,59 @@ const registeredComponents = ['Counter', 'Hello', 'Nav'].forEach(comp => {
 })
 
 /* eslint-disable max-len */
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: [...componentEntries, './example/index.tsx'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[hash].js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
+module.exports = [
+  {
+		name: "vendor",
+		// mode: "development || "production",
+		mode: "production",
+    entry: Object.keys(packages.dependencies).filter(name => name.indexOf('@types') < 0).concat(['react', 'react-dom', 'react-router', 'react-router-dom', 'reselect']),
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "vendor.js",
+			library: "vendor_[hash]"
+		},
+		plugins: [
+			new webpack.DllPlugin({
+				name: "vendor_[hash]",
+				path: path.resolve(__dirname, "dist/manifest.json")
+			})
+		]
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './example/index.html',
-      inject: true,
-    }),
-  ],
+  {
+    name: 'app',
+    mode: 'development',
+    dependencies: ['vendor'],
+    devtool: 'inline-source-map',
+    entry: {
+      app: ['./example/index.tsx', ...componentEntries]
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].[hash].js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    },
 
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './example/index.html',
+        inject: false,
+        chunks: ['app'],
+        jsassets: [`/vendor.js`],
+      }),
+      new webpack.DllReferencePlugin({
+				manifest: manifest
+			}),
     ],
+
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: 'ts-loader' },
+      ],
+    },
   },
-}
+
+]
