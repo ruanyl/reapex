7517724fb9dbe5a9e4248860dedea190967b9fe8
Yulong Ruan <ruanyu1@gmail.com>
2021-05-29 12:16:42 +0300

    update & cleanup deps

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,24 @@
 import { AnyAction, applyMiddleware, createStore, Middleware, Reducer, Store } from 'redux'
 
+let composeWithDevTools: any
+
+if (process.env.NODE_ENV === 'development') {
+  try {
+    composeWithDevTools = require('redux-devtools-extension').composeWithDevTools
+  } catch (e) {
+    console.warn('You may want: redux-devtools-extension(https://github.com/zalmoxisus/redux-devtools-extension)')
+  }
+}
+
 export const configureStore = (
   reducers: Reducer,
   middlewares: Middleware[],
   initialState?: Record<string, any>
 ): Store<any, AnyAction> => {
   let store: Store<any, AnyAction>
-  if (process.env.NODE_ENV === 'development') {
-    try {
-      require.resolve('redux-devtools-extension')
-      const composeWithDevTools = require('redux-devtools-extension').composeWithDevTools
-      store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middlewares)))
-    } catch (e) {
-      store = createStore(reducers, initialState, applyMiddleware(...middlewares))
-    }
+  if (composeWithDevTools) {
+    composeWithDevTools = require('redux-devtools-extension').composeWithDevTools
+    store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middlewares)))
   } else {
     store = createStore(reducers, initialState, applyMiddleware(...middlewares))
   }
